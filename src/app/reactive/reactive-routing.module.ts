a6c6b2e2392edc75008eca4d00cca9e2ad060e73
Jhosef <jmperezperez25@gmail.com>
2024-11-14 06:13:07 -0500

    adding routes

---
+++ src/app/reactive/reactive-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BasicPageComponent } from './pages/basicPage/basicPage.component';
+import { DynamicPageComponent } from './pages/dynamicPage/dynamicPage.component';
+import { SwitchesPageComponent } from './pages/switchesPage/switchesPage.component';
+
+const routes: Routes = [
+
+  {
+    path:'' ,
+    children:[
+      {
+        path: 'basic', component: BasicPageComponent ,
+      } ,
+      {
+        path: 'dynamic', component: DynamicPageComponent ,
+      } ,
+      {
+        path: 'switches', component: SwitchesPageComponent ,
+      } ,
+      {
+        path: '**', component: BasicPageComponent ,
+      } ,
+    ]
+
+  }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ReactiveRoutingModule { }
