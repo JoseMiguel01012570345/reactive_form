249ed3583e770e6ad487d75950bd99bc01f2c6c3
Jhosef <jmperezperez25@gmail.com>
2024-11-14 08:36:38 -0500

    extending form group to validations in input box

--- src/app/reactive/pages/basicPage/basicPage.component.ts
+++ src/app/reactive/pages/basicPage/basicPage.component.ts
@@ -8,16 +8,39 @@ import { FormBuilder, FormControl, FormGroup , Validators } from '@angular/forms
 })
 export class BasicPageComponent {
 
-    public myForm: any
+    public myForm: FormGroup
 
     constructor( private fb: FormBuilder){
     
         this.myForm = this.fb.group({
             name: ['' , [ Validators.required , Validators.minLength(3) ]] ,
-            price: [0 , ] ,
+            price: [0 , [Validators.required , Validators.min(0)] ] ,
             inStorage: [0 , [Validators.required , Validators.min(0)]] ,
         })
     }
 
+    isValidField( field:string ): boolean | null {            
+        
+        return this.myForm.controls[field].errors && 
+        this.myForm.controls[field].touched
+    } 
 
+    getFieldError(field:string):string | null {
+        
+        if( !this.myForm.controls[field]) return null
+
+        const errors = this.myForm.controls[field].errors || {} 
+
+        for (const key of Object.keys(errors)) {
+            switch(key){
+                case 'required':
+                return 'this field is required'
+                case 'minlength':
+                    return `minimum required ${errors['minlength'].requiredLength} caracters.`
+                case 'min':
+                    return 'minimum is 0'
+            }
+        }
+        return null
+    }
  }
